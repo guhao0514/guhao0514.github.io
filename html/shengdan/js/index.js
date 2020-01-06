b48797f088c51623cee4eb69c913249e9ce84b0f
guhao0514 <18210184891@163.com>
2020-01-06 19:05:36 +0800

    111

--- html/shengdan/js/index.js
+++
@@ -1,9 +0,0 @@
-// Audio Player
-var audio;
-
-function initAudioPlayer(){
-       audio = new Audio();      audio.src="";
-       audio.loop = true;
-       audio.play(); 
-}
-window.addEventListener("load", initAudioPlayer);
